ff1306b37c585ad996c9a66aae321a3005c47039
Alan Ortega <isc.alanortega@gmail.com>
2025-11-10 22:13:15 -0600

    Se agrega el Data_Tramites que hará las peticiones a la API

---
+++ SantiagoConectaIA/SantiagoConectaFront/Data/Data_Tramites.cs
@@ -0,0 +1,42 @@
+﻿using EngramaCoreStandar.Results;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using SantiagoConectaIA.Share.Objects.TramitesModule;
+using SantiagoConectaIA.Share.PostModels.TramitesModule;
+using System.Net;
+using System.Text;
+
+namespace SantiagoConectaFront.Data
+{
+	public class Data_Tramites
+	{
+		private readonly HttpClient _HttpClient;
+		private readonly NavigationManager _navigator;
+		public Data_Tramites(HttpClient httpClient, NavigationManager navigator)
+		{
+			_HttpClient = httpClient;
+			_navigator = navigator;
+			_HttpClient.DefaultRequestHeaders.Add("Accept", "text/plain");
+			_HttpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+		}
+		public async Task<Response<List<Tramite>>> PostGetAllTramites(PostGetTramites data)
+		{
+			var url = "/api/Tramites/PostGetTramites";
+			var request = new HttpRequestMessage(HttpMethod.Post, new Uri(_HttpClient.BaseAddress!, url));
+			request.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+
+			var respuesta = await _HttpClient.SendAsync(request);
+
+			
+			if (respuesta.StatusCode == HttpStatusCode.BadRequest)
+			{
+				return null;
+			}
+
+			var json = await respuesta.Content.ReadAsStringAsync();
+			var resultado = JsonConvert.DeserializeObject<Response<List<Tramite>>>(json);
+			return resultado;
+		}
+
+	}
+}
--- SantiagoConectaIA/SantiagoConectaFront/Program.cs
+++ SantiagoConectaIA/SantiagoConectaFront/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SantiagoConectaFront;
+using SantiagoConectaFront.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -19,6 +20,5 @@ else
 	builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["PRODUCTION_API_URL"]!), Timeout = TimeSpan.FromMinutes(10) });
 }
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<Data_Tramites>();
 await builder.Build().RunAsync();
